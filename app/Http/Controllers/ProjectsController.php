661347ed1374837a5bd45861437b0165e803414a
Aivaras Karaliūnas <karaliunas.aivaras@gmail.com>
2022-01-26 10:03:20 +0200

    Added Projects admin functionality (CRUD)

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Projects;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $projects = Projects::latest()->paginate(5);
+
+        return view('projects.index', compact('projects'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('projects.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required'
+        ]);
+
+        Projects::create($request->all());
+
+        return redirect()->route('projects.index')
+            ->with('success', 'Project created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Projects  $projects
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Projects $projects)
+    {
+        return view('projects.show', compact('projects'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Projects  $projects
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Projects $projects)
+    {
+        return view('projects.edit', compact('projects'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Projects  $projects
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Projects $projects)
+    {
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required'
+        ]);
+
+        $projects->update($request->all());
+
+        return redirect()->route('projects.index')
+            ->with('success', 'Project updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Projects  $projects
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Projects $projects)
+    {
+        $projects->delete();
+
+        return redirect()->route('projects.index')
+            ->with('success', 'Project deleted successfully');
+    }
+}
